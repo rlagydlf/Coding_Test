d17634df9185052c16448d7169ca462566b116f5
rlagydlf <01021139593a@gmail.com>
2024-10-18 10:48:09 +0900

    [level 0] Title: 대문자와 소문자, Time: 15.84 ms, Memory: 82.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        for(int i = 0; i<my_string.length(); i++){
+             char c = my_string.charAt(i);
+            if(c>='a' && c<='z') c -= 32;
+            else c += 32;
+
+            answer = (String )(answer + c);
+        }
+        return answer;
+    }
+}
